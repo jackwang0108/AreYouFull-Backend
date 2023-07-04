08670f97352c4dde09809b7226e49821a2fc50d3
Jack <jack4shihong@gmail.com>
2023-07-04 09:30:35 +0800

    init via idea

---
+++ src/main/java/com/ayf/areyoufull/AreYouFullBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ayf.areyoufull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreYouFullBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AreYouFullBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayf/areyoufull/AreYouFullBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayf.areyoufull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreYouFullBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
